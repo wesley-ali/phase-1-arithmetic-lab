055cb75a6bbf559e85a75302f5ca843220a54370
Wesley Alinyo <wesleyalinyo@gmail.com>
2024-03-28 13:08:40 +0300

    adds arithmetic index.js

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+const num1 = 31; 
+const num2 = 2; 
+const multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random() * 100) + 1;
+const num3 = 20; 
+const num4 = 8;
+const mod = num3 % num4;
+
+
+const numberSet = [10, 20, 15, 8, 20]; 
+const max = Math.max(...numberSet); 
+
+console.log("Multiply:", multiply);
+console.log("Random:", random);
+console.log("Mod:", mod);
+console.log("Max:", max);
